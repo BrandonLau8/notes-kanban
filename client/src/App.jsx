2f58e2a60aa6a28a78106209e9e6390a2d11776c
Brandon Lau <brandonlau8888@gmail.com>
2023-11-04 18:33:22 -0400

    231104 - add create user to db

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,74 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Notes from "./components/Notes";
 
 const App = () => {
-  return(
-    <>
-    <div style={{display:'grid', backgroundColor:'', justifyContent:'center'}}>
-      <form>
-        <label>username</label>
-      <input />
-      </form>
-      <form>
-        <label>password</label>
-      <input />
-      </form>
-      <section>
-        <button>
-          Login
-        </button>
-      </section>
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
 
-    </div>
-    {/* <Notes /> */}
+  const login = async () => {
+    try {
+      const response = await axios.post("http://localhost:3001/createuser", {
+        username: user,
+        password: password,
+      });
+      if (response.status === 201) {
+        alert("User Added");
+      } else {
+        console.error("Unexpected status:", response.status);
+        alert("Failed to add user. Check console for details.");
+      }
+    } catch (error) {
+      console.error("Error adding user:", error);
+      alert("Failed to add user");
+    }
+  };
+
+  const changeUserInput = (e) => {
+    setUser(e.target.value);
+  };
+
+  const changePasswordInput = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    login(user, password);
+  };
+
+  return (
+    <>
+      <div
+        style={{
+          display: "grid",
+          backgroundColor: "",
+          justifyContent: "center",
+        }}
+      >
+        <form>
+          <label>username</label>
+          <input
+            type="text"
+            value={user}
+            placeholder="Username"
+            onChange={changeUserInput}
+          />
+        </form>
+        <form>
+          <label>password</label>
+          <input
+            type="password"
+            value={password}
+            placeholder="Password"
+            onChange={changePasswordInput}
+          />
+        </form>
+        <section>
+          <button onClick={submitForm}>Login</button>
+        </section>
+      </div>
+      {/* <Notes /> */}
     </>
   );
 };
--- server/index.js
+++ server/index.js
@@ -1,95 +1,133 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mysql = require('mysql2');
-const cors = require('cors');
-require('dotenv').config()
+const mysql = require("mysql2/promise");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 
-const DB_HOST = process.env.DB_HOST
-const DB_USER = process.env.DB_USER
-const DB_PASSWORD = process.env.DB_PASSWORD
-const DB_DATABASE = process.env.DB_DATABASE
-const DB_PORT = process.env.DB_PORT
+const DB_HOST = process.env.DB_HOST;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_DATABASE = process.env.DB_DATABASE;
+const DB_PORT = process.env.DB_PORT;
 
 const db = mysql.createPool({
-    connectionLimit: 100,
-    user: DB_USER,
-    host: DB_HOST,
-    password: DB_PASSWORD,
-    database: DB_DATABASE,
-    port: DB_PORT
+  connectionLimit: 100,
+  user: DB_USER,
+  host: DB_HOST,
+  password: DB_PASSWORD,
+  database: DB_DATABASE,
+  port: DB_PORT,
 });
 
-const port = process.env.PORT
+const port = process.env.PORT;
 
-
+//Connect to DB
 db.getConnection((err, connection) => {
-    if (err) throw (err)
-    console.log('DB connected succesfully:' + connection.threadId)
-})
-
-app.post('/create', (req, res) => {
-    
-    const input = req.body.input;
-    const content = req.body.content;
-    
-    db.query(
-        `INSERT INTO kanban (input, content) VALUES (?,?)`,
-        [input, content],
-        (err, result) => {
-            if(err) {
-                console.log(err);
-            } else {
-                res.send('Values Inserted')
-            }
-        }
-    );
-    console.log(req.body);
+  if (err) throw err;
+  console.log("DB connected succesfully:" + connection.threadId);
+  connection.release();
 });
 
-app.get('/boxes', (req, res) => {
-    db.query('SELECT * FROM kanban', (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.send(result);
-        }
-    });
-});
+//Create USER
+app.post("/createuser", async (req, res) => {
+  const username = req.body.username;
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
-app.put('/update', (req, res) => {
-    console.log(req.body)
-    const content = req.body.content;
-    const input = req.body.input;
-        db.query(
-        'UPDATE kanban SET content = ? WHERE input = ?',
-        [content, input],
-        (err, result) => {
-            if (err) {
-                console.log(err);
-            } else {
-                res.send(result);
-            }
-        }
-    );
-});
+  const connection = await db.getConnection();
 
-app.delete('/delete/:input', (req, res) => {
-    const input = req.params.input;
-    console.log(req.params)
-    db.query(
-        'DELETE FROM kanban WHERE input = ?', 
-        input, 
-        (err, result) => {
-            if (err) {
-                console.log(err);
-            } else {
-                res.send(result);
-            }
-        });
+  try {
+    const sqlSearch = "SELECT * FROM kanban.usertable WHERE username = ?";
+    const search_query = mysql.format(sqlSearch, [username]);
+    const sqlInsert = "INSERT INTO kanban.usertable VALUES (0,?,?)";
+    const insert_query = mysql.format(sqlInsert, [username, hashedPassword]);
+
+    const [result] = await connection.query(search_query);
+
+    console.log("------> Search Results");
+    console.log(result.length);
+
+    if (result.length != 0) {
+      connection.release();
+      console.log("------> User already exists");
+      res.sendStatus(409);
+    } else {
+      await connection.query(insert_query);
+      connection.release();
+      console.log("--------> Created new User");
+      res.sendStatus(201);
+    }
+  } catch (error) {
+    connection.release();
+    console.error("Error:", error);
+    res.sendStatus(500);
+  }
 });
 
 
-app.listen(port, () => console.log(`Server Started on port ${port}...` ))
+//Add to DB
+app.post("/create", (req, res) => {
+  const input = req.body.input;
+  const content = req.body.content;
+
+  db.query(
+    `INSERT INTO kanban (input, content) VALUES (?,?)`,
+    [input, content],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("Values Inserted");
+      }
+    }
+  );
+  console.log(req.body);
+});
+
+//Get from DB
+app.get("/boxes", (req, res) => {
+  db.query("SELECT * FROM kanban", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+//Update DB
+app.put("/update", (req, res) => {
+  console.log(req.body);
+  const content = req.body.content;
+  const input = req.body.input;
+  db.query(
+    "UPDATE kanban SET content = ? WHERE input = ?",
+    [content, input],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
+
+//Delete DB
+app.delete("/delete/:input", (req, res) => {
+  const input = req.params.input;
+  console.log(req.params);
+  db.query("DELETE FROM kanban WHERE input = ?", input, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+//Start Server
+app.listen(port, () => console.log(`Server Started on port ${port}...`));
