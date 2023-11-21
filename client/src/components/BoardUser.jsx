5132f067cb2c3e48018f4e2847fd7f6cd204b64c
Brandon Lau <brandonlau8888@gmail.com>
2023-11-21 01:07:06 -0500

    231121 - working on admin board

--- client/src/components/BoardUser.jsx
+++ client/src/components/BoardUser.jsx
@@ -1,4 +1,4 @@
-
+import React, {useEffect, useState} from 'react'
 import UserService from "../services/user.service";
 
 const BoardUser = () => {
--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -8,103 +8,106 @@ const Op = db.Sequelize.Op; //use sequalize library
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
-exports.signup = async (req, res) => {
+exports.signup = (req, res) => {
   //Save User to database
-  try {
-    const user = await User.create({
-      username: req.body.username,
-      email: req.body.email,
-      //complete hashing process before allowing program to move on to the next line
-      // salt round of 8
-      password: bcrypt.hashSync(req.body.password, 8),
-    });
+
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    //complete hashing process before allowing program to move on to the next line
+    // salt round of 8
+    password: bcrypt.hashSync(req.body.password, 8),
+  })
 
     //If roles are provided in the request body,
     //find all roles in the database where the name matches any of the roles specified.
-    if (req.body.roles) {
-      const roles = await Role.findAll({
-        where: {
-          name: {
-            //OR operation checking for roles where name is one of the values specifed in req body
-            [Op.or]: req.body.roles,
-          },
-        },
-      });
-
-      const result = user.setRoles(roles);
-      if (result) res.send({ message: "User registered successfully!" });
-    } else {
-      //user has role = 1
-      const result = user.setRoles([1]);
-      if (result) res.send({ message: "User registered successfully!" });
-    }
-  } catch (error) {
-    res.status(500).send({ message: error.message });
-  }
+    .then((user) => {
+      if (req.body.roles) {
+        Role.findAll({
+          where: {
+            name: {
+              [Op.or]: req.body.roles,
+            }
+          }
+        }).then((roles) => {
+          user.setRoles(roles).then(() => {
+            res.send({ message: "User Mod Admin was registered successfully!" });
+          });
+        });
+      } else {
+        //user has role = 1
+        user.setRoles([1]).then(() => {
+        res.send({ message: "User registered successfully!" });
+        })
+      }
+    })
+    .catch((error) => {
+      res.status(500).send({ message: error.message });
+    });
 };
 
-exports.signin = async (req, res) => {
-  try {
-    const user = await User.findOne({
-      where: {
-        username: req.body.username,
-      },
-    });
-
-    if (!user) {
-      res.status(400).send({ message: "User not found." });
-    }
-
-    const passwordIsvalid = bcrypt.compareSync(
-      req.body.password,
-      user.password
-    );
-
-    if (!passwordIsvalid) {
-      return res.status(401).send({
-        message: "Invalid Password!",
-      });
-    }
-
-    const token = jwt.sign(
-      { id: user.id }, //payload of jwt
-      config.secret, //secret key to sign jwt to verify integrity of token
-      {
-        algorithm: "HS256", //commonly used hashing algo to sign token
-        //alows for key sizes smaller than 256 bits. safer to use stronger key size
-        allowInsecureKeySizes: true,
-        expiresIn: 86400, // 24 hours in seconds
+exports.signin = (req, res) => {
+  User.findOne({
+    where: {
+      username: req.body.username,
+    },
+  })
+    .then((user) => {
+      if (!user) {
+        res.status(404).send({ message: "User not found." });
       }
-    );
 
-    //Layout roles associated with the user
-    let authorities = [];
-    const roles = await user.getRoles();
-    for (let i = 0; i < roles.length; i++) {
-      //Uppercase common convention that use Spring Security or similar framework
-      authorities.push("ROLE_" + roles[i].name.toUpperCase());
-    }
+      const passwordIsvalid = bcrypt.compareSync(
+        req.body.password,
+        user.password
+      );
 
-    //Generate token
-    req.session.token = token;
-    return res.status(200).send({
-      id: user.id,
-      username: user.username,
-      email: user.email,
-      roles: authorities,
+      if (!passwordIsvalid) {
+        return res.status(401).send({
+          accessToken: null,
+          message: "Invalid Password!",
+        });
+      }
+
+      const token = jwt.sign(
+        { id: user.id }, //payload of jwt
+        config.secret, //secret key to sign jwt to verify integrity of token
+        {
+          algorithm: "HS256", //commonly used hashing algo to sign token
+          //alows for key sizes smaller than 256 bits. safer to use stronger key size
+          allowInsecureKeySizes: true,
+          expiresIn: 86400, // 24 hours in seconds
+        }
+      );
+
+      //Layout roles associated with the user
+      let authorities = [];
+      user.getRoles().then((roles) => {
+        for (let i = 0; i < roles.length; i++) {
+          //Uppercase common convention that use Spring Security or similar framework
+          authorities.push("ROLE_" + roles[i].name.toUpperCase());
+        }
+        res.status(200).send({
+          id: user.id,
+          username: user.username,
+          email: user.email,
+          roles: authorities,
+          accessToken: token,
+        });
+      });
+    })
+    .catch((error) => {
+      res.status(500).send({ message: error.message });
     });
-  } catch (error) {
-    return res.status(500).send({ message: error.message });
-  }
 };
 
 exports.signout = async (req, res) => {
-    try{
-        req.session = null;
-        return res.status(200).send({
-            message: "You've been signed out!"
-        });
-    } catch(err) {
-        this.next(err)
-    }
-}
+  try {
+    req.session = null;
+    return res.status(200).send({
+      message: "You've been signed out!",
+    });
+  } catch (err) {
+    this.next(err);
+  }
+};
