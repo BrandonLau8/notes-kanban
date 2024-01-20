86b552ff9a194e9238858063268cc00354fbc1a3
Brandon Lau <brandonlau8888@gmail.com>
2024-01-19 22:02:03 -0500

    240119 - added mui navbar adding mui sidenavbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,82 +1,85 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { AppBar, Box } from '@mui/material';
+import React from "react";
+import { Link as RouterLink} from "react-router-dom";
 
-const Navbar = ({ isAuthenticated, showModeratorBoard, showAdminBoard, currentUser, logOut }) => {
-    // console.log("isAuthenticated:", isAuthenticated);
-    // console.log("showModeratorBoard:", showModeratorBoard);
-    // console.log("showAdminBoard:", showAdminBoard);
-    // console.log("currentUser:", currentUser);
-    return (
-      <Box sx={{flexGrow:1}}>
-        <AppBar position='static'>
-    <nav className="navbar navbar-expand navbar-dark bg-dark">
-    <Link to={"/"} className="navbar-brand">
-      Kanban Notes
-    </Link>
-    <div className="navbar-nav mr-auto">
-      <li className="nav-item">
-        <Link to={"/home"} className="nav-link">
-          Home
-        </Link>
-      </li>
+import { AppBar, Box, Toolbar, Link } from "@mui/material";
 
-      {showModeratorBoard && (
-        <li className="nav-item">
-          <Link to={"/mod"} className="nav-link">
-            Moderator Board
+const Navbar = ({
+  isAuthenticated,
+  showModeratorBoard,
+  showAdminBoard,
+  currentUser,
+  logOut,
+}) => {
+  // console.log("isAuthenticated:", isAuthenticated);
+  // console.log("showModeratorBoard:", showModeratorBoard);
+  // console.log("showAdminBoard:", showAdminBoard);
+  // console.log("currentUser:", currentUser);
+  return (
+    <Box sx={{flexGrow:1}}>
+      <AppBar position="static">
+        <Toolbar>
+          {/* <nav className="navbar navbar-expand navbar-dark bg-dark"> */}
+          <Link component={RouterLink} to={"/"} sx={{color: 'white', flexGrow:1}}>
+            Kanban Notes
           </Link>
-        </li>
-      )}
+          <div className="navbar-nav mr-auto">
+            <Link component={RouterLink} to={"/home"} className="nav-link">
+              Home
+            </Link>
 
-      {showAdminBoard && (
-        <li className="nav-item">
-          <Link to={"/admin"} className="nav-link">
-            Admin Board
-          </Link>
-        </li>
-      )}
+            {showModeratorBoard && (
+              <li className="nav-item">
+                <Link component={RouterLink} to={"/mod"} className="nav-link">
+                  Moderator Board
+                </Link>
+              </li>
+            )}
 
-      {isAuthenticated && (
-        <li className="nav-item">
-          <Link to={"/user"} className="nav-link">
-            User
-          </Link>
-        </li>
-      )}
-    </div>
+            {showAdminBoard && (
+              <li className="nav-item">
+                <Link component={RouterLink} to={"/admin"} className="nav-link">
+                  Admin Board
+                </Link>
+              </li>
+            )}
 
-    {isAuthenticated ? (
-      <div className="navbar-nav ml-auto">
-        <li className="nav-item">
-          <Link to={`/profile/${currentUser?.id}`} className="nav-link">
-            {currentUser.username}
-          </Link>
-        </li>
-        <li className="nav-item">
-          <a href="/login" className="nav-link" onClick={logOut}>
-            LogOut
-          </a>
-        </li>
-      </div>
-    ) : (
-      <div className="navbar-nav ml-auto">
-        <li className="nav-item">
-          <Link to={"/login"} className="nav-link">
-            Login
-          </Link>
-        </li>
+            {isAuthenticated && (
+              <li className="nav-item">
+                <Link component={RouterLink} to={"/user"} className="nav-link">
+                  User
+                </Link>
+              </li>
+            )}
+          </div>
 
-        <li className="nav-item">
-          <Link to={"/register"} className="nav-link">
-            Sign Up
-          </Link>
-        </li>
-      </div>
-    )}
-  </nav>
-  </AppBar>
-  </Box>
+          {isAuthenticated ? (
+            <div className="navbar-nav ml-auto">
+              
+                <Link component={RouterLink} to={`/profile/${currentUser?.id}`} className="nav-link">
+                  {currentUser.username}
+                </Link>
+              
+              
+                <a href="/login" className="nav-link" onClick={logOut}>
+                  LogOut
+                </a>
+              
+            </div>
+          ) : (
+            <Box>
+              <Link component={RouterLink} to={"/login"} className="nav-link" sx={{color: 'white'}}>
+                Login
+              </Link>
+
+              <Link component={RouterLink} to={"/register"} className="nav-link" sx={{color: 'white', ml:3}}>
+                Sign Up
+              </Link>
+            </Box>
+          )}
+          {/* </nav> */}
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
 
